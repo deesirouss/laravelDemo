f5f91b6620d1cc32e9b84ce09ea1006175f15340
deesirouss <deesirouss@gmail.com>
2022-05-19 16:56:26 +0545

    done

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ssh -o StrictHostKeyChecking=no ubuntu@${{ secrets.KNOWN_HOSTS }}
-echo Start deploy
-cd ~/laravelDemo/
-git pull origin master
-cp .env.example .env
-sudo docker compose up --build -d
-echo Deploy end
